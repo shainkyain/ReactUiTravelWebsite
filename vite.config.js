64e7b372727494078271ca456d9d7a52fc5c7577
Shainky Jain <jainshainky123@gmail.com>
2024-08-02 11:55:01 +0530

    deploy update

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-
+  build: {
+    outDir: 'dist', // Directory for the build output
+  },
   plugins: [react()],
   base: '/ReactUiTravelWebsite/'
 })
